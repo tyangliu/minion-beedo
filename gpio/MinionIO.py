c6a7a390b6405be5cddb0d82eb0a1767b87fa4bb
Thomas Liu <thomas@one45.com>
2015-08-12 12:06:17 -0700

    Attempt to remove then add event detect after 1 second

--- gpio/MinionIO.py
+++ gpio/MinionIO.py
@@ -32,12 +32,16 @@ class MinionIO:
         GPIO.add_event_detect(in_channels[0], GPIO.RISING, callback=self.input_cb, bouncetime=200)
         GPIO.add_event_detect(in_channels[1], GPIO.RISING, callback=self.input_cb, bouncetime=200)
 
+    @gen.coroutine
     def input_cb(self, channel):
         print('input received', channel)
+        GPIO.remove_event_detect(channel)
         if channel == self.in_button_a:
             self.signal(self.out_alarm)
         elif channel == self.in_button_b:
             self.signal(self.out_speech)
+        gen.sleep(1)
+        GPIO.add_event_detect(channel, GPIO.RISING, callback=self.input_cb, bouncetime=200)
 
     @staticmethod
     @gen.coroutine
